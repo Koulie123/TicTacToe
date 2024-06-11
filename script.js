9da9698a794ca836dfde1521a0c596e6ea6e1c59
Koulie <kouliekyriakos@hotmail.com>
2024-06-11 06:55:25 -0400

    gamebaord function added

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-const gameBoaord = (function() {
-    let gameGrid = [];
-    const createGameGrid = function(){
-        gameGrid = [];
-        for(i; i < 3; i++){
-            for(j; j<3; j++) {
+
+function gameBoard() {
+    const createGameBoard = () => {
+        let gameGrid = [];
+        for(let i = 0; i < 3; i++){
+            for(let j = 0; j<3; j++) {
                 gameGrid.push({});
             }
         }
         return gameGrid;
     }
-});
+    let turnNumber = 1;
+    const increaseTurn = () => turnNumber++;
+    const getTurnNumber = () => turnNumber; 
+    const getTurnType = () => {
+        if (turnNumber % 2 == 0) return "X";
+        else return "O";
+    }
+return { createGameBoard, increaseTurn, getTurnNumber, getTurnType};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Game loop starts here
+let gameBoardObject = gameBoard();
+console.log(gameBoardObject.createGameBoard());
+console.log("")
